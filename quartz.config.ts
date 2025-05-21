56cfa0a4ca90fa5b70063e1b5df39abea49be261
Deivis44 <deivis_working.delta571@slmail.me>
2025-05-21 14:18:04 -0500

    Quartz sync: May 21, 2025, 2:18 PM

--- quartz.config.ts
+++ quartz.config.ts
@@ -16,7 +16,7 @@ const config: QuartzConfig = {
       provider: "plausible",
     },
     locale: "en-US",
-    baseUrl: "quartz.jzhao.xyz",
+    baseUrl: "https://deivis-blog.deivid-24-zzz.workers.dev/",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "modified",
     theme: {
